7f626edecf6d270efbd6edd2e967fc31c465237f
ProgrammerLP <developer.programmerlp@gmx.net>
2023-03-09 09:13:37 +0100

    Added the first Methods

--- PLP-SystemInfo/ComponentInfo.cs
+++ PLP-SystemInfo/ComponentInfo.cs
@@ -1,10 +1,153 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Management;
 using System.Text;
 
 namespace PLP_SystemInfo
 {
     internal class ComponentInfo
     {
+        public static string GetOperatingSystemInfo()
+        {
+            var osn = "";
+            var osa = "";
+
+            var mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+
+            foreach (ManagementObject managementObject in mos.Get())
+            {
+                if (managementObject["Caption"] != null)
+                {
+                    osn = managementObject["Caption"].ToString();   //Display operating system caption
+                }
+
+                if (managementObject["OSArchitecture"] != null)
+                {
+                    osa = managementObject["OSArchitecture"].ToString();   //Display operating system architecture.
+                }
+            }
+
+            return osn + "(" + osa + ")";
+        }
+
+        public static string GetCPUName()
+        {
+            var processor_name = Registry.LocalMachine.OpenSubKey(@"Hardware\Description\System\CentralProcessor\0", RegistryKeyPermissionCheck.ReadSubTree);
+
+            if (processor_name != null)
+            {
+                if (processor_name.GetValue("ProcessorNameString") != null)
+                {
+                    try
+                    {
+                        return processor_name.GetValue("ProcessorNameString").ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            return "Error by getting the processor name";
+        }
+
+        public static int GetCPUThreads() => Environment.ProcessorCount;
+
+        public static int GetCPUCores()
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("Select * from Win32_Processor");
+            int cores = 0;
+            foreach (ManagementObject item in mos.Get())
+            {
+                cores = int.Parse(item["NumberOfCores"].ToString());
+            }
+
+            return cores;
+        }
+
+        public static long GetInstalledRAMSize()
+        {
+            var oMs = new ManagementScope();
+            var oQuery = new ObjectQuery("SELECT Capacity FROM Win32_PhysicalMemory");
+            var oSearcher = new ManagementObjectSearcher(oMs, oQuery);
+            var oCollection = oSearcher.Get();
+
+            long memSize = 0;
+            long mCap = 0;
+
+            // In case more than one Memory sticks are installed
+            foreach (ManagementObject obj in oCollection)
+            {
+                mCap = Convert.ToInt64(obj["Capacity"]);
+                memSize += mCap;
+            }
+
+            memSize /= 1073741824;
+
+            return memSize;
+        }
+
+        public static long GetTotalUsableRam()
+        {
+            return 0;
+        }
+
+        public static long GetRamInUse()
+        {
+            return 0;
+        }
+
+        public static long GetAvailableRam()
+        {
+
+            return 0;
+        }
+
+        public static string GetGraphicscardName()
+        {
+            string name = "";
+
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DisplayControllerConfiguration");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    name = queryObj["Name"].ToString();
+                }
+
+                return name;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred while querying GPU Name in NuGet Package PLP-SystemInfo: " + e.Message);
+                return e.Message;            }
+        }
+
+        public static long GetGraphicscardVRAM()
+        {
+            long totalVRAM = 0;
+
+            try
+            {
+                ManagementClass c = new ManagementClass("Win32_VideoController");
+                foreach (ManagementObject o in c.GetInstances())
+                {
+                    string gpuTotalMem = String.Format("{0} ", o["AdapterRam"]);
+                    long vram = Convert.ToInt64(gpuTotalMem);
+                    Console.WriteLine(vram / 1073741824 + "Gb");
+                    totalVRAM = vram / 1073741824;
+                }
+                return totalVRAM;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred while querying GPU VRAM in NuGet Package PLP-SystemInfo: " + e.Message);
+                return -1;
+            }
+        }
     }
 }
--- PLP-SystemInfo/SystemInfo.cs
+++ PLP-SystemInfo/SystemInfo.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace PLP_SystemInfo
 {
     internal class SystemInfo
     {
-        
+        public static bool IsDarkModeEnabled => Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "AppsUseLightTheme", -1) is null;
+
+        public static string GetWindowsAccentColor()
+        {
+            Color color = GetAccentColor();
+            string c = "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+            return c;
+        }
+
+        public static Color GetAccentColor()
+        {
+            const String DWM_KEY = @"Software\Microsoft\Windows\DWM";
+            using (RegistryKey dwmKey = Registry.CurrentUser.OpenSubKey(DWM_KEY, RegistryKeyPermissionCheck.ReadSubTree))
+            {
+                const String KEY_EX_MSG = "The \"HKCU\\" + DWM_KEY + "\" registry key does not exist.";
+                if (dwmKey is null) throw new InvalidOperationException(KEY_EX_MSG);
+
+                Object accentColorObj = dwmKey.GetValue("AccentColor");
+                if (accentColorObj is Int32 accentColorDword)
+                {
+                    return ParseDWordColor(accentColorDword);
+                }
+                else
+                {
+                    const String VALUE_EX_MSG = "The \"HKCU\\" + DWM_KEY + "\\AccentColor\" registry key value could not be parsed as an ABGR color.";
+                    throw new InvalidOperationException(VALUE_EX_MSG);
+                }
+            }
+
+        }
+
+        private static Color ParseDWordColor(Int32 color)
+        {
+            Byte
+                a = (byte)((color >> 24) & 0xFF),
+                b = (byte)((color >> 16) & 0xFF),
+                g = (byte)((color >> 8) & 0xFF),
+                r = (byte)((color >> 0) & 0xFF);
+
+            return Color.FromArgb(a, r, g, b);
+        }
     }
 }
